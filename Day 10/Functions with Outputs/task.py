b447cdde1c8d77c2ac1e213f58d9f0ecba50fb60
henrique.souza <henrique.sl959@hotmail.com>
2026-04-27 14:21:14 -0300

    day 10 atualizado

---
+++ Day 10/Functions with Outputs/task.py
@@ -0,0 +1,5 @@
+def format_name():
+    f_name = str(input("f_name: "))
+    l_name = str(input("l_name: "))
+    return f_name + " " + l_name
+
--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -4,3 +4,32 @@
 # TODO-4: Compare bids in dictionary
 
 
+func = True
+
+dici = {}
+
+current = 0
+
+while func:
+
+    name = input("Type your name: \n")
+    bid = int(input("Type your bid: \n"))
+    if bid > current:
+        current = bid
+
+    question = input("Here are others who need to bid? 'yes' or 'no'?")
+
+    dici[name] = bid
+
+    print("\n" * 20)
+
+    if question == 'no':
+        func = False
+        print("bye")
+
+
+for key, value in dici.items():
+    if value == current:
+        print(f"The highest bid is: \nname: {key}\nbid: {value}")
+
+
