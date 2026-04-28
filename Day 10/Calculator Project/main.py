6512b1c3a0418e79b8fc56ab32c813acf6a98451
henrique.souza <henrique.sl959@hotmail.com>
2026-04-28 17:40:55 -0300

    day 10 atualizado

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,31 @@
+import art
+# print(art.logo)
+
+first = int(input("Type the first number: \n"))
+operator = input("Type the mathematical operator ")
+second = int(input("Type the second number: \n"))
+
 def add(n1, n2):
     return n1 + n2
 
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+calc = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+print(calc["+"](first, second))
+
+# calculadora = add
+# print(calculadora(5, 5))
 
