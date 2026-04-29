91a6ad32b291b7b2769ff5be7d6103b8efd2f651
henrique.souza <henrique.sl959@hotmail.com>
2026-04-29 16:51:31 -0300

    day 10 finalizado

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,9 +1,12 @@
 import art
 # print(art.logo)
 
-first = int(input("Type the first number: \n"))
-operator = input("Type the mathematical operator ")
-second = int(input("Type the second number: \n"))
+first = int(input("Type the first number: "))
+operator = str(input("\n+\n-\n*\n/\nPick an operator: "))
+second = int(input("Type the second number: "))
+sumNum = 0
+
+working = True
 
 def add(n1, n2):
     return n1 + n2
@@ -24,7 +27,21 @@ calc = {
     "/": divide
 }
 
-print(calc["+"](first, second))
+print(first, operator, second,"=", calc[operator](first, second))
+result = calc[operator](first, second)
+
+while working:
+    ask = input("Do you want to continue with the previous result? Type 'y' or 'n': \n")
+    if ask == "y":
+        operator = str(input("\n+\n-\n*\n/\nPick an operator: "))
+        nextNum = int(input("What's the next number? \n"))
+        sumNum = calc[operator](result, nextNum)
+        result = sumNum
+        print(result)
+    else:
+        print(f"Result: {sumNum}")
+        print("Bye")
+        working = False
 
 # calculadora = add
 # print(calculadora(5, 5))
